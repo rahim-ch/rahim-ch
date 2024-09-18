6981bce3aef320b75ba02f04b80a079cdf94731e
AbdERRAHIM Chabbi <116936569+rahim-ch@users.noreply.github.com>
2024-09-18 13:37:27 +0100

    errrrrrrrrrr

---
+++ src/components/Skills.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="bg-gray-100 py-12 text-center">
+      <h2 class="text-4xl font-semibold mb-8">My Skills</h2>
+      <div class="grid grid-cols-2 md:grid-cols-4 gap-8 px-4">
+       
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        skills: ['Vue.js', 'JavaScript', 'Tailwind CSS', 'HTML5', 'CSS3', 'Git', 'Node.js'],
+      };
+    },
+  };
+  </script>
+  
